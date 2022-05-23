cc355cbcf2d74f2129da290e58f04d71c4a614ee
Alamin <alaminhussain32@gmail.com>
2022-05-23 14:22:07 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './Pages/Home/Home';
+import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
     <div>
+      <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
       </Routes>
