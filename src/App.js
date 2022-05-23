e8fe7fdf2a167c088f7794661cc8f3a725136fca
Alamin <alaminhussain32@gmail.com>
2022-05-23 14:44:43 +0600

    login added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Home from './Pages/Home/Home';
 import Navbar from './Pages/Shared/Navbar';
 import Footer from './Pages/Shared/Footer';
 import NotFound from './Pages/NotFound/NotFound';
+import Blogs from './Pages/Blogs/Blogs';
+import Login from './Pages/Login/Login';
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
       <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
+        <Route path='/login' element={<Login></Login>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
