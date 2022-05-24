773a22965c7c30abb9cf9d8ba51239fb6856566f
Alamin <alaminhussain32@gmail.com>
2022-05-24 12:32:50 +0600

    mongodb connected

--- src/Pages/Products/Products.js
+++ src/Pages/Products/Products.js
@@ -4,7 +4,7 @@ import Product from './Product';
 const Products = ({ home }) => {
     const [products, setProducts] = useState([])
     useEffect(() => {
-        fetch('products.json')
+        fetch('http://localhost:5000/products')
             .then(res => res.json())
             .then(data => setProducts(data))
     }, [])
@@ -13,20 +13,21 @@ const Products = ({ home }) => {
             <h1 className='text-center py-8 text-5xl text-cyan-400 font-bold'>Our Product Collection</h1>
             <div>
                 <div className='grid lg:grid-cols-3 sm:grid-cols-1  gap-5  px-5'>
-                    {home ?
-                        products.slice(6, 10).reverse().map(product => (
-                            <Product
-                                key={product._id}
-                                product={product}
-                            ></Product>
-                        ))
-                        :
-                        products.map(product => (
-                            <Product
-                                key={product._id}
-                                product={product}
-                            ></Product>
-                        ))
+                    {
+                        home ?
+                            products.slice(6, 10).reverse().map(product => (
+                                <Product
+                                    key={product._id}
+                                    product={product}
+                                ></Product>
+                            ))
+                            :
+                            products.map(product => (
+                                <Product
+                                    key={product._id}
+                                    product={product}
+                                ></Product>
+                            ))
                     }
                 </div>
             </div>
